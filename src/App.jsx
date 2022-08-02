401db16bf04c7fcc96665f3149149dccdc736e56
Bere <bre_kc073@hotmail.com>
2022-08-01 19:45:44 -0500

    add styles in navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,50 @@
 import './App.css';
-import React from 'react';
-import {Nav, NavItem, NavLink} from 'reactstrap'
+import {
+  Collapse, 
+  Navbar, 
+  NavbarToggler, 
+  NavbarBrand, 
+  Nav, 
+  NavItem, 
+  NavLink, 
+  UncontrolledDropdown,
+  DropdownToggle, 
+  DropdownMenu, 
+  DropdownItem, 
+  NavbarText
+} from 'reactstrap';
 
 function App() {
   return (
-    <Nav
-      fill
-      justified
-    >
-      <NavItem>
-        <NavLink
-          active
-          href="#"
-        >
-          Link
-        </NavLink>
-      </NavItem>
-      <NavItem>
-        <NavLink href="#">
-          Another Link
-        </NavLink>
-      </NavItem>
-      <NavItem>
-        <NavLink
-          disabled
-          href="#"
-        >
-          Disabled Link
-        </NavLink>
-      </NavItem>
-    </Nav>
+    <div>
+      <Navbar expand='md' color='dark' container='xl' dark>
+        <NavbarBrand href="#">Productos</NavbarBrand>
+        <NavbarBrand href="#">Agregar Productos</NavbarBrand>
+        <NavbarBrand href="#">Historial de compras</NavbarBrand>
+        <NavbarBrand href="#">Carrito de compras</NavbarBrand>
+        <NavbarToggler></NavbarToggler>
+        <Collapse expand='md' color='dark' container='xl' dark>
+          <Nav>
+            <NavItem>
+              <NavLink href="#">Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#">GitHub</NavLink>
+            </NavItem>
+            <UncontrolledDropdown>
+              <DropdownToggle> Options </DropdownToggle>
+              <DropdownMenu>
+                <DropdownItem> Option 1 </DropdownItem>
+                <DropdownItem> Option 2 </DropdownItem>
+                <DropdownItem />
+                <DropdownItem> Reset </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+          <NavbarText>Simple Text</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
   );
 }
 
